1dde917d2d7960b4a7c5ce3963c2299c78370e3f
habibur <va.habibur@gmail.com>
2023-05-19 21:39:31 +0600

    added add product page and add product to server

--- src/components/SingleToy.jsx
+++ src/components/SingleToy.jsx
@@ -18,21 +18,6 @@ const SingleToy = ({ car }) => {
             </div>
         </div>
 
-
-
-
-
-        // <div className="card w-96 bg-base-100 shadow-xl">
-        //     <figure><img src={image_url} alt="Shoes" /></figure>
-        //     <div className="card-body">
-        //         <h2 className="card-title">{name}</h2>
-        //         <p>Rating: {rating}</p>
-        //         <p>Price: {price}</p>
-        //         <div className="card-actions ">
-        //             <button className="btn btn-primary">View Details</button>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
