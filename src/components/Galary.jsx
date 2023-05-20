65c11e87ed36fa7a8a3d145522cf2885fb2370b6
habibur <va.habibur@gmail.com>
2023-05-20 22:28:56 +0600

    change galary image

--- src/components/Galary.jsx
+++ src/components/Galary.jsx
@@ -1,4 +1,9 @@
-import galary from '../assets/galary.jpg'
+import galary1 from '../assets/Galary/galary-1.jpg'
+import galary2 from '../assets/Galary/galary-2.jpg'
+import galary3 from '../assets/Galary/galary-3.jpg'
+import galary4 from '../assets/Galary/galary-4.jpg'
+import galary5 from '../assets/Galary/galary-5.jpg'
+import galary6 from '../assets/Galary/galary-6.jpg'
 
 const Galary = () => {
     return (
@@ -9,34 +14,34 @@ const Galary = () => {
                         <div className="w-1/2 p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+                                src={galary1} />
                         </div>
                         <div className="w-1/2 p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+                                src={galary2} />
                         </div>
                         <div className="w-full p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+                                src={galary3} />
                         </div>
                     </div>
                     <div className="flex w-1/2 flex-wrap">
                         <div className="w-full p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+                                src={galary4} />
                         </div>
                         <div className="w-1/2 p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+                                src={galary5} />
                         </div>
                         <div className="w-1/2 p-1 md:p-2 hover:scale-110 duration-500">
                             <img alt="gallery"
                                 className="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+                                src={galary6} />
                         </div>
                     </div>
                 </div>
